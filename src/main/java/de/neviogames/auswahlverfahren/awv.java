07cacbff9e11500be31282e971d31240c20dc70d
Nevio <nevio@neviogames.de>
2022-08-26 20:14:55 +0200

    New config system

--- src/main/java/de/neviogames/auswahlverfahren/awv.java
+++ src/main/java/de/neviogames/auswahlverfahren/awv.java
@@ -6,7 +6,6 @@ import de.neviogames.auswahlverfahren.commands.auswahlCommand;
 import de.neviogames.auswahlverfahren.commands.reloadCommand;
 import de.neviogames.auswahlverfahren.utils.Configuration;
 import de.neviogames.auswahlverfahren.utils.database;
-import de.neviogames.auswahlverfahren.utils.edits.command;
 import lombok.Getter;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -27,28 +26,31 @@ public class awv extends JavaPlugin {
     private final List<String> authors = this.getDescription().getAuthors();
 
     @Override
-    public void onEnable() {
+    public void onLoad() {
         getLogger().info("-------------------------------");
-        getLogger().info("Lade "+name+"");
+        getLogger().info("Setting up " +this.name);
         instance = this;
 
-        //Load Config
-        saveDefaultConfig();
-        if(Configuration.getInstance().load()) {
-            getLogger().info("Config geladen.");
-        } else getLogger().warning("Config wurde nicht geladen!");
+
+        getLogger().info("Loading configurations...");
+        // Load configuration
+        loadConfiguration();
+
+        getLogger().info("-------------------------------");
+    }
+
+    @Override
+    public void onEnable() {
+        getLogger().info("-------------------------------");
+        getLogger().info("Lade "+this.name+"");
 
         //Load MySQL
-        database.createTable();
-        database.createTableFormerEventCandidates();
+        createMysqlTables();
 
         //Load Commands
         registerCMD();
 
-        getLogger().info("Plugin " + name + " Version " + version + " erfolgreich geladen");
-        getLogger().info("by " + authors.toString().replace("[","").replace("]",""));
-        getLogger().info("Plugin Aktiviert");
-        getLogger().info("-------------------------------");
+        logEndMessage("Loaded");
     }
 
     @Override
@@ -57,10 +59,27 @@ public class awv extends JavaPlugin {
         instance = null;
         getServer().getServicesManager().unregisterAll(this);
         Bukkit.getScheduler().cancelTasks(this);
-        getLogger().info("Plugin " + name + " Version " + version + " entladen");
-        getLogger().info("by " + authors.toString().replace("[","").replace("]",""));
-        getLogger().info("Plugin Deaktiviert");
+        logEndMessage("Unloaded");
+    }
+
+    private void logEndMessage(String message) {
+        getLogger().info("Plugin " + this.name + " Version " + this.version + " " + message);
+        getLogger().info("by " + this.authors.toString().replace("[","").replace("]",""));
         getLogger().info("-------------------------------");
+    }
+
+    public void loadConfiguration() {
+        saveDefaultConfig();
+        if(Configuration.getInstance().load()) {
+            getLogger().info("Config geladen.");
+        } else {
+            getLogger().warning("Config wurde nicht geladen!");
+        }
+    }
+
+    public void createMysqlTables() {
+        database.createTable();
+        database.createTableFormerEventCandidates();
     }
 
     private void registerCMD() {
--- src/main/java/de/neviogames/auswahlverfahren/utils/Configuration.java
+++ src/main/java/de/neviogames/auswahlverfahren/utils/Configuration.java
@@ -2,11 +2,16 @@ package de.neviogames.auswahlverfahren.utils;
 
 import de.neviogames.auswahlverfahren.awv;
 import de.neviogames.nglib.utils.io.ErrorHandle;
+import de.neviogames.nglib.utils.utility;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -15,38 +20,76 @@ public final class Configuration {
     @Getter
     private static final Configuration instance = new Configuration();
 
-    private boolean isEnabled;
-    private boolean dagiPlay;
+    private boolean isApplicationPhase;
     private String eventName;
     private boolean denyFormerCandidates;
-    private boolean secondTeam;
-    private String selectionText1;
-    private String selectionText2;
-    private int teamSize1;
-    private int teamSize2;
-    private boolean onlyWizards;
+
+    private String teamSelectMode;
+    private Map<String, NGEventTeam> teams;
+    private List<NGEventGroup> groups;
+
+    private Map<String, List<NGEventFixedPlayer>> fixedPlayers;
 
     // load values from config
     public boolean load() {
         try {
             FileConfiguration configuration = awv.getPlugin(awv.class).getConfig();
-            isEnabled = configuration.getBoolean("settings.enable");
-            dagiPlay = configuration.getBoolean("settings.dagiPlay");
+            isApplicationPhase = configuration.getBoolean("settings.applicationPhase");
             eventName = configuration.getString("settings.eventName");
             denyFormerCandidates = configuration.getBoolean("settings.denyFormerCandidates");
-            secondTeam = configuration.getBoolean("settings.secondTeam");
-            selectionText1 = ChatColor.translateAlternateColorCodes('&', configuration.getString("settings.selectionText1"));
-            selectionText2 = ChatColor.translateAlternateColorCodes('&', configuration.getString("settings.selectionText2"));
-            teamSize1 = configuration.getInt("settings.teamSize1");
-            teamSize2 = configuration.getInt("settings.teamSize2");
-            onlyWizards = configuration.getBoolean("settings.onlyWizards");
+
+            teamSelectMode = configuration.getString("settings.teamSelectMode").toLowerCase();
+            configuration.getConfigurationSection("settings.teams").getKeys(false).forEach(team -> {
+                if (!configuration.getBoolean("settings.teams."+team+".enabled")) return;
+                teams.put(team, new NGEventTeam(
+                        team,
+                        configuration.getString("settings.teams." + team + ".display"),
+                        configuration.getString("settings.teams." + team + ".color")
+                ));
+            });
+
+            configuration.getConfigurationSection("settings.groups").getKeys(false).forEach(group -> {
+                if (!configuration.getBoolean("settings.groups."+group+".enabled")) return;
+                if (!utility.isNumber(group)) throw new NumberFormatException();
+                groups.add(new NGEventGroup(
+                        Integer.parseInt(group),
+                        configuration.getInt("settings.groups." + group + ".groupSize"),
+                        configuration.getString("settings.groups." + group + ".selectionText")
+                ));
+            });
+
+            configuration.getConfigurationSection("settings.fixedPlayers").getKeys(false).forEach(player -> {
+                if (!configuration.getBoolean("settings.fixedPlayers."+player+".enabled")) return;
+                final String team = configuration.getString("settings.fixedPlayers." + player + ".team");
+
+                List<NGEventFixedPlayer> fp = fixedPlayers.getOrDefault(team, new ArrayList<>());
+                fp.add(new NGEventFixedPlayer(
+                        UUID.fromString(player),
+                        team,
+                        configuration.getInt("settings.fixedPlayers." + player + ".group")
+                ));
+                fixedPlayers.put(team, fp);
+            });
+
 
             return true;
         } catch (Throwable t) {
             ErrorHandle.error(ErrorHandle.cs, t, awv.getPrefix());
             return false;
         }
+    }
 
+    public int getTeamSelectModeId() {
+        switch (this.teamSelectMode) {
+            case "rank":
+                return -1;
+            case "perm":
+                return 1;
+            case "select":
+                return 2;
+            default: // = "auto"
+                return 0;
+        }
     }
 
 }
